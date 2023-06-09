6807aec824ec092e607e3a85e9e8150ae3497a95
Jericho Garcia <gjericho6@gmail.com>
2023-06-09 22:05:45 +0800

    as

--- InventorySystemGrocery/AddCategory.Designer.cs
+++ InventorySystemGrocery/AddCategory.Designer.cs
@@ -30,12 +30,12 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddCategory));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.btnCatExit = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.txtCategory = new System.Windows.Forms.TextBox();
             this.btnAddCat = new System.Windows.Forms.Button();
             this.btnClearCat = new System.Windows.Forms.Button();
-            this.btnCatExit = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -45,9 +45,24 @@
             this.panel1.Controls.Add(this.btnCatExit);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(435, 63);
+            this.panel1.Size = new System.Drawing.Size(652, 97);
             this.panel1.TabIndex = 0;
+            // 
+            // btnCatExit
+            // 
+            this.btnCatExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCatExit.FlatAppearance.BorderSize = 0;
+            this.btnCatExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCatExit.Image = ((System.Drawing.Image)(resources.GetObject("btnCatExit.Image")));
+            this.btnCatExit.Location = new System.Drawing.Point(616, 0);
+            this.btnCatExit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnCatExit.Name = "btnCatExit";
+            this.btnCatExit.Size = new System.Drawing.Size(36, 35);
+            this.btnCatExit.TabIndex = 36;
+            this.btnCatExit.UseVisualStyleBackColor = true;
+            this.btnCatExit.Click += new System.EventHandler(this.btnCatExit_Click);
             // 
             // label1
             // 
@@ -55,9 +70,10 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(90, 9);
+            this.label1.Location = new System.Drawing.Point(135, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(259, 42);
+            this.label1.Size = new System.Drawing.Size(380, 64);
             this.label1.TabIndex = 5;
             this.label1.Text = "Add Category";
             // 
@@ -66,18 +82,20 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.DarkCyan;
-            this.label2.Location = new System.Drawing.Point(12, 101);
+            this.label2.Location = new System.Drawing.Point(18, 155);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(137, 23);
+            this.label2.Size = new System.Drawing.Size(203, 35);
             this.label2.TabIndex = 1;
             this.label2.Text = "Category Name:";
             // 
             // txtCategory
             // 
             this.txtCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtCategory.Location = new System.Drawing.Point(146, 98);
+            this.txtCategory.Location = new System.Drawing.Point(219, 151);
+            this.txtCategory.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtCategory.Name = "txtCategory";
-            this.txtCategory.Size = new System.Drawing.Size(267, 26);
+            this.txtCategory.Size = new System.Drawing.Size(398, 35);
             this.txtCategory.TabIndex = 2;
             // 
             // btnAddCat
@@ -87,9 +105,10 @@
             this.btnAddCat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnAddCat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAddCat.ForeColor = System.Drawing.Color.White;
-            this.btnAddCat.Location = new System.Drawing.Point(207, 141);
+            this.btnAddCat.Location = new System.Drawing.Point(310, 217);
+            this.btnAddCat.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnAddCat.Name = "btnAddCat";
-            this.btnAddCat.Size = new System.Drawing.Size(56, 25);
+            this.btnAddCat.Size = new System.Drawing.Size(84, 38);
             this.btnAddCat.TabIndex = 34;
             this.btnAddCat.Text = "ADD";
             this.btnAddCat.UseVisualStyleBackColor = false;
@@ -102,38 +121,28 @@
             this.btnClearCat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnClearCat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnClearCat.ForeColor = System.Drawing.Color.White;
-            this.btnClearCat.Location = new System.Drawing.Point(278, 140);
+            this.btnClearCat.Location = new System.Drawing.Point(417, 215);
+            this.btnClearCat.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnClearCat.Name = "btnClearCat";
-            this.btnClearCat.Size = new System.Drawing.Size(56, 25);
+            this.btnClearCat.Size = new System.Drawing.Size(84, 38);
             this.btnClearCat.TabIndex = 35;
             this.btnClearCat.Text = "CLEAR";
             this.btnClearCat.UseVisualStyleBackColor = false;
             this.btnClearCat.Click += new System.EventHandler(this.btnClearCat_Click);
             // 
-            // btnCatExit
-            // 
-            this.btnCatExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnCatExit.FlatAppearance.BorderSize = 0;
-            this.btnCatExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnCatExit.Image = ((System.Drawing.Image)(resources.GetObject("btnCatExit.Image")));
-            this.btnCatExit.Location = new System.Drawing.Point(411, 0);
-            this.btnCatExit.Name = "btnCatExit";
-            this.btnCatExit.Size = new System.Drawing.Size(24, 23);
-            this.btnCatExit.TabIndex = 36;
-            this.btnCatExit.UseVisualStyleBackColor = true;
-            this.btnCatExit.Click += new System.EventHandler(this.btnCatExit_Click);
-            // 
             // AddCategory
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(434, 196);
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(651, 302);
             this.Controls.Add(this.btnClearCat);
             this.Controls.Add(this.btnAddCat);
             this.Controls.Add(this.txtCategory);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "AddCategory";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Load += new System.EventHandler(this.AddCategory_Load);
--- InventorySystemGrocery/CategoryList.cs
+++ InventorySystemGrocery/CategoryList.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,12 +13,31 @@ namespace InventorySystemGrocery
 {
     public partial class CategoryList : Form
     {
+        SqlConnection connect = new SqlConnection();
+        SqlCommand command = new SqlCommand();
+        DatabaseConnection dbcon = new DatabaseConnection();
+        SqlDataReader reader;
         public CategoryList()
         {
             InitializeComponent();
         }
 
-    
+        public void loadCategory()
+        {
+            int i = 0;
+            DGVCategory.Rows.Clear();
+            command = new SqlCommand("Select * from Category", connect);
+            connect.Open();
+            reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                i += 1;
+                DGVCategory.Rows.Add(i, reader["CategoryID"].ToString(), reader["CategoryName"].ToString());
+            }
+            reader.Close();
+            connect.Close();
+        }
 
         private void btnCatExit_Click(object sender, EventArgs e)
         {
@@ -26,7 +46,13 @@ namespace InventorySystemGrocery
 
         private void DGVCategory_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-
+            string colname = DGVCategory.Columns[e.ColumnIndex].Name;
+            
+            if(colname == "Edit")
+            {
+                AddCategory addCat = new AddCategory();
+                
+            }
         }
     }
 }
