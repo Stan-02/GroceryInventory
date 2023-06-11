492c1e091adfb0158301585aa669998ad78aa352
Stan-02 <cincorhylle@gmail.com>
2023-06-11 23:23:50 +0800

    deleters editers

--- InventorySystemGrocery/ProductStock.Designer.cs
+++ InventorySystemGrocery/ProductStock.Designer.cs
@@ -29,8 +29,8 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProductStock));
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.label2 = new System.Windows.Forms.Label();
             this.txtSearch = new System.Windows.Forms.TextBox();
             this.purchaseSearch = new System.Windows.Forms.Button();
@@ -38,6 +38,7 @@
             this.DGVPurchaselist = new System.Windows.Forms.DataGridView();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
+            this.btnDelete = new System.Windows.Forms.Button();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -61,20 +62,18 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(752, 28);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(393, 18);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(267, 64);
+            this.label2.Size = new System.Drawing.Size(180, 42);
             this.label2.TabIndex = 23;
             this.label2.Text = "Inventory";
             // 
             // txtSearch
             // 
             this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSearch.Location = new System.Drawing.Point(62, 14);
-            this.txtSearch.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtSearch.Location = new System.Drawing.Point(41, 9);
             this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(488, 35);
+            this.txtSearch.Size = new System.Drawing.Size(327, 26);
             this.txtSearch.TabIndex = 12;
             this.txtSearch.Text = "Search";
             // 
@@ -84,10 +83,9 @@
             this.purchaseSearch.FlatAppearance.BorderSize = 0;
             this.purchaseSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.purchaseSearch.Image = ((System.Drawing.Image)(resources.GetObject("purchaseSearch.Image")));
-            this.purchaseSearch.Location = new System.Drawing.Point(6, 5);
-            this.purchaseSearch.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.purchaseSearch.Location = new System.Drawing.Point(4, 3);
             this.purchaseSearch.Name = "purchaseSearch";
-            this.purchaseSearch.Size = new System.Drawing.Size(46, 57);
+            this.purchaseSearch.Size = new System.Drawing.Size(31, 37);
             this.purchaseSearch.TabIndex = 12;
             this.purchaseSearch.UseVisualStyleBackColor = true;
             // 
@@ -96,9 +94,10 @@
             this.panel1.BackColor = System.Drawing.Color.LightGray;
             this.panel1.Controls.Add(this.DGVPurchaselist);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(0, 200);
+            this.panel1.Location = new System.Drawing.Point(0, 130);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1946, 722);
+            this.panel1.Size = new System.Drawing.Size(1080, 468);
             this.panel1.TabIndex = 20;
             // 
             // DGVPurchaselist
@@ -106,14 +105,14 @@
             this.DGVPurchaselist.AllowUserToAddRows = false;
             this.DGVPurchaselist.BackgroundColor = System.Drawing.Color.White;
             this.DGVPurchaselist.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
-            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle5.BackColor = System.Drawing.Color.Aquamarine;
-            dataGridViewCellStyle5.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.DGVPurchaselist.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Aquamarine;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DGVPurchaselist.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.DGVPurchaselist.ColumnHeadersHeight = 34;
             this.DGVPurchaselist.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
             this.DGVPurchaselist.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
@@ -128,36 +127,35 @@
             this.Column8,
             this.Edit,
             this.Delete});
-            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle6.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.Cyan;
-            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.DGVPurchaselist.DefaultCellStyle = dataGridViewCellStyle6;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.Cyan;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.DGVPurchaselist.DefaultCellStyle = dataGridViewCellStyle2;
             this.DGVPurchaselist.Dock = System.Windows.Forms.DockStyle.Fill;
             this.DGVPurchaselist.EnableHeadersVisualStyles = false;
             this.DGVPurchaselist.Location = new System.Drawing.Point(0, 0);
-            this.DGVPurchaselist.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.DGVPurchaselist.Name = "DGVPurchaselist";
             this.DGVPurchaselist.RowHeadersVisible = false;
             this.DGVPurchaselist.RowHeadersWidth = 62;
             this.DGVPurchaselist.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.DGVPurchaselist.Size = new System.Drawing.Size(1946, 722);
+            this.DGVPurchaselist.Size = new System.Drawing.Size(1080, 468);
             this.DGVPurchaselist.TabIndex = 17;
             this.DGVPurchaselist.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DGVPurchaselist_CellContentClick);
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.DarkCyan;
+            this.panel2.Controls.Add(this.btnDelete);
             this.panel2.Controls.Add(this.label2);
             this.panel2.Controls.Add(this.panel3);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1946, 200);
+            this.panel2.Size = new System.Drawing.Size(1080, 130);
             this.panel2.TabIndex = 19;
             // 
             // panel3
@@ -165,11 +163,20 @@
             this.panel3.BackColor = System.Drawing.Color.White;
             this.panel3.Controls.Add(this.txtSearch);
             this.panel3.Controls.Add(this.purchaseSearch);
-            this.panel3.Location = new System.Drawing.Point(14, 128);
-            this.panel3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel3.Location = new System.Drawing.Point(9, 83);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(566, 63);
+            this.panel3.Size = new System.Drawing.Size(377, 41);
             this.panel3.TabIndex = 18;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Image = ((System.Drawing.Image)(resources.GetObject("btnDelete.Image")));
+            this.btnDelete.Location = new System.Drawing.Point(1036, 79);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(32, 46);
+            this.btnDelete.TabIndex = 24;
+            this.btnDelete.UseVisualStyleBackColor = true;
             // 
             // Column1
             // 
@@ -239,30 +246,33 @@
             this.Column8.HeaderText = "Expiration Date";
             this.Column8.MinimumWidth = 8;
             this.Column8.Name = "Column8";
-            this.Column8.Width = 166;
+            this.Column8.Width = 104;
             // 
             // Edit
             // 
             this.Edit.HeaderText = "";
+            this.Edit.Image = ((System.Drawing.Image)(resources.GetObject("Edit.Image")));
             this.Edit.MinimumWidth = 8;
             this.Edit.Name = "Edit";
-            this.Edit.Width = 150;
+            this.Edit.Width = 50;
             // 
             // Delete
             // 
             this.Delete.HeaderText = "";
+            this.Delete.Image = ((System.Drawing.Image)(resources.GetObject("Delete.Image")));
             this.Delete.MinimumWidth = 8;
             this.Delete.Name = "Delete";
-            this.Delete.Width = 150;
+            this.Delete.Width = 50;
             // 
             // ProductStock
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1946, 922);
+            this.ClientSize = new System.Drawing.Size(1080, 598);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "ProductStock";
             this.Text = "ProductStock";
             this.panel1.ResumeLayout(false);
@@ -295,5 +305,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
         private System.Windows.Forms.DataGridViewImageColumn Edit;
         private System.Windows.Forms.DataGridViewImageColumn Delete;
+        private System.Windows.Forms.Button btnDelete;
     }
 }
